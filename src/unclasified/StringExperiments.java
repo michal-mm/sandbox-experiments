8b3bdc33771fa4175cdbf25c8640876c40a618a2
michal-mm <michal.miotk@gmail.com>
2025-11-18 13:46:19 +0100

    Experiment with String.transform method and learning what it is good for.

---
+++ src/unclasified/StringExperiments.java
@@ -0,0 +1,29 @@
+package unclasified;
+
+public class StringExperiments {
+
+    private static String shout(String input) {
+        return "!!! " + input.toUpperCase() + " !!!";
+    }
+
+    void main() {
+        String s = "hello world";
+        var shouted = s.transform(StringExperiments::shout);
+        IO.println(shouted);
+        IO.println(s);
+
+        IO.println("=======");
+        String longString = """
+                This is a block of text
+                spread across several lines.
+                It would be really painful to
+                call specific methods right after
+                this string (unless you
+                call the variable name directly),
+                hence we will use transform method!
+                """;
+
+        var shoutedLong = longString.transform(StringExperiments::shout);
+        IO.println(shoutedLong);
+    }
+}
