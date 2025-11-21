06c55c188fa0921e5e19e850c0d887569334a8cc
michal-mm <michal.miotk@gmail.com>
2025-11-21 09:55:31 +0100

    Experiment with counting words ina file using stream, collector with grouping by string -> string and Collectors.counting

--- src/unclassified/FilesExperiments.java
+++ src/unclassified/FilesExperiments.java
@@ -6,6 +6,7 @@ import java.nio.file.Path;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class FilesExperiments {
 
@@ -18,5 +19,14 @@ public class FilesExperiments {
                 .forEach(elem-> words.put(elem, words.getOrDefault(elem, 0)+1));
 
         words.forEach((key, value) -> IO.println("'" + key + "' -> " + value));
+
+        IO.println("======");
+        var frequency = Files.readAllLines(Path.of("src/unclassified/FilesExperiments.java"))
+                .stream()
+                .flatMap(line -> Arrays.stream(line.split("\\s+")))
+                .filter(str -> !str.isEmpty())
+                .collect(Collectors.groupingBy(str -> str, Collectors.counting()));
+
+        frequency.forEach((key, value) -> IO.println("'" + key + "' -> " + value));
     }
 }
