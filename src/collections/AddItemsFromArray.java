3710b562dc5ab19f39c13fa9a3dc9bda06ef6e7e
michal-mm <michal.miotk@gmail.com>
2025-12-05 16:52:57 +0100

    Experiments with overflow for Integer and Long by adding 1 to their max values

--- src/collections/AddItemsFromArray.java
+++ src/collections/AddItemsFromArray.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class AddItemsFromArray {
 
@@ -20,5 +20,30 @@ public class AddItemsFromArray {
     void simpleAddTest() {
         int[] tab = {1,2,3,4};
         assertEquals(10, addItems(tab));
+    }
+
+    @Test
+    void testMaxInt() {
+        int tab [] = {1, Integer.MAX_VALUE};
+        long result = addItems(tab);
+        try {
+            int intFromLong = Math.toIntExact(result);
+            IO.println(intFromLong);
+            fail();
+        } catch (ArithmeticException _) {
+            assertTrue(true);
+        }
+
+        int i = 1;
+        i += Integer.MAX_VALUE;
+        IO.println("i=" + i);
+        IO.println("min int=" + Integer.MIN_VALUE);
+        assertEquals(Integer.MIN_VALUE, i);
+
+        long l = 1;
+        l += Long.MAX_VALUE;
+        IO.println(l);
+        IO.println(Long.MIN_VALUE);
+        assertEquals(Long.MIN_VALUE, l);
     }
 }
