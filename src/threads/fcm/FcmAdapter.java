f0294ad1b50aefce7152df8c9a6c54a1db9e520e
michal-mm <michal.miotk@gmail.com>
2026-03-18 20:34:36 +0100

    starting experiment with adapter and virtual threads

---
+++ src/threads/fcm/FcmAdapter.java
@@ -0,0 +1,21 @@
+package threads.fcm;
+
+import java.util.List;
+import java.util.Set;
+
+public class FcmAdapter {
+
+    private void performAction(List<String> actions, Set<String> params){
+        for (var action : actions){
+
+        }
+    }
+
+    private void successfulActionWithRandomSleep(String action, String param) {
+        // TODO
+    }
+
+    private void randomFailAction(String action, String param) {
+        // TODO
+    }
+}
