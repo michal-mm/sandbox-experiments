698b9b882317c1034b67c15a239b598eace21022
michal-mm <michal.miotk@gmail.com>
2026-03-19 18:56:05 +0100

    added basic methods for running actions

--- src/threads/fcm/ActionAdapter.java
+++ src/threads/fcm/ActionAdapter.java
@@ -1,21 +1,40 @@
 package threads.fcm;
 
+import java.time.Duration;
 import java.util.List;
 import java.util.Set;
+import java.util.random.RandomGenerator;
 
 public class ActionAdapter {
 
-    private void performAction(List<String> actions, Set<String> params){
-        for (var action : actions){
+    @FunctionalInterface
+    public interface ActionOperator {
+        String apply(String action, String response, Duration sleepTime);
+    }
 
+
+    private void performAction(List<String> actions, List<String> params){
+        for (var action : actions){
+            var sleepTime = Duration.ofSeconds(RandomGenerator.getDefault().nextInt(3, 10));
+            if (RandomGenerator.getDefault().nextBoolean()) {
+                executeAction(action, params.get(RandomGenerator.getDefault().nextInt(params.size())), sleepTime, this::successfulActionWithRandomSleep);
+            } else {
+                executeAction(action, params.get(RandomGenerator.getDefault().nextInt(params.size())), sleepTime, this::randomFailAction);
+            }
         }
     }
 
-    private void successfulActionWithRandomSleep(String action, String param) {
-        // TODO
+    private String executeAction(String action, String response, Duration sleepTimeOfSeconds, ActionOperator op){
+        return op.apply(action, response, sleepTimeOfSeconds);
     }
 
-    private void randomFailAction(String action, String param) {
+    private String successfulActionWithRandomSleep(String action, String param, Duration sleepTimeOfSeconds) {
         // TODO
+        return null;
+    }
+
+    private String randomFailAction(String action, String param, Duration sleepTimeOfSeconds) {
+        // TODO
+        return null;
     }
 }
